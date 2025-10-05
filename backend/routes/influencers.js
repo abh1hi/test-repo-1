80bac1a0bdecafa03e0bf21759fefd7c4c66e110
abh1hi <162477075+abh1hi@users.noreply.github.com>
2025-10-05 15:01:26 +0530

    Delete backend directory

--- backend/routes/influencers.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getInfluencers, getInfluencerById } = require('../controllers/influencerController');
-
-router.get('/', getInfluencers);
-router.get('/:id', getInfluencerById);
-
-module.exports = router;
